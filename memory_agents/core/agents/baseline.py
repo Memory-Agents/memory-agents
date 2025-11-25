570f8cb87354b2c55d04726e8cb5afc276f7568d
st170001 <st170001@stud.uni-stuttgart.de>
2025-11-25 20:52:12 +0900

    ruff format .

--- memory_agents/core/agents/baseline.py
+++ memory_agents/core/agents/baseline.py
@@ -1,11 +1,8 @@
 from langchain.agents import create_agent
-from langgraph.checkpoint.memory import InMemorySaver 
+from langgraph.checkpoint.memory import InMemorySaver
 
 agent = create_agent(
     model="gpt-4o-mini",
     system_prompt="You are a memory agent that helps the user to solve tasks.",
     checkpointer=InMemorySaver(),
 )
-
-
-
--- memory_agents/main.py
+++ memory_agents/main.py
@@ -1,20 +1,24 @@
+import asyncio
 from dotenv import load_dotenv
+
 load_dotenv()
 
 from core.run_agent import run_agent
-from core.agents.baseline import agent 
-import asyncio
+from core.agents.baseline import agent
+
+
 
 async def main():
     print("Hello from memory-agents!")
     print("In order to run agents import and execute the according `run()` function.")
     print("This is a test execution of the baseline agent:")
-    
+
     response = await run_agent(agent, "Hello World!")
     print("Response from baseline agent:", response)
 
     response = await run_agent(agent, "What have i said in my first message?")
     print("Response from baseline agent:", response)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
