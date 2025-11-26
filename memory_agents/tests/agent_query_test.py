cbb00ad046ab31af44064d69c1bfcdd117b52c0c
st170001 <st170001@stud.uni-stuttgart.de>
2025-11-26 15:08:21 +0900

    Remove benchmark results

--- memory_agents/tests/agent_query_test.py
+++ memory_agents/tests/agent_query_test.py
@@ -12,7 +12,8 @@ async def test_query_baseline_agent():
     from memory_agents.core.agents.baseline import BaselineAgent
 
     baseline_agent = BaselineAgent()
-    response = await run_agent(baseline_agent.agent, "Hello World!", "1")
+    thread_id = "1"
+    response = await run_agent(baseline_agent.agent, "Hello World!", thread_id)
     assert response is not None
     assert isinstance(response, str)
 
@@ -22,6 +23,7 @@ async def test_init_graphiti_agent_code():
     from memory_agents.core.agents.graphiti import GraphitiAgent
 
     graphit_agent = await GraphitiAgent.create()
-    response = await run_agent(graphit_agent.agent, "Hello World!", "1")
+    thread_id = "1"
+    response = await run_agent(graphit_agent.agent, "Hello World!", thread_id)
     assert response is not None
     assert isinstance(response, str)
