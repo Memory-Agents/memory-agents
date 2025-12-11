6c522ab6a9a8284095ab4d324c61f0c024b17a8a
st170001 <st170001@stud.uni-stuttgart.de>
2025-12-11 19:33:09 +0900

    Adjusted pdoc command in workflow

--- memory_agents/longmemeval/parse_result.py
+++ memory_agents/longmemeval/parse_result.py
@@ -1,14 +1,16 @@
 import json
 
+try:
+    result = open("my_predictions_oracle.jsonl.eval-results-gpt-4o", "r")
+    output = open("subset.txt", "w")
 
-result = open("my_predictions_oracle.jsonl.eval-results-gpt-4o", "r")
-output = open("subset.txt", "w")
+    for line in result:
+        data = line.strip()
+        data = json.loads(data)
+        if data["autoeval_label"]["label"] == False:
+            output.write(data["question_id"] + "\n")
 
-for line in result:
-    data = line.strip()
-    data = json.loads(data)
-    if data["autoeval_label"]["label"] == False:
-        output.write(data["question_id"] + "\n")
-
-output.close()
-result.close()
+    output.close()
+    result.close()
+except:
+    print("Result not available, could not parse result")
